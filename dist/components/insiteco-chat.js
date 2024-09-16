f7379aceb9020a0bb3d7a1680213d167a1880887
Antique <33049468+Charlotte-z@users.noreply.github.com>
2024-09-16 20:58:21 +0800

    change dist

---
+++ dist/components/insiteco-chat.js
@@ -0,0 +1,6 @@
+import { I as InsitecoChat$1, d as defineCustomElement$1 } from './p-468fc91d.js';
+
+const InsitecoChat = InsitecoChat$1;
+const defineCustomElement = defineCustomElement$1;
+
+export { InsitecoChat, defineCustomElement };
---
+++ dist/components/insiteco-input.js
@@ -0,0 +1,6 @@
+import { I as InsitecoInput$1, d as defineCustomElement$1 } from './p-3d18b500.js';
+
+const InsitecoInput = InsitecoInput$1;
+const defineCustomElement = defineCustomElement$1;
+
+export { InsitecoInput, defineCustomElement };
